e9429a717910097385571e586c951c8f19e0c8d1
Mads Bach <FentoNqq@hotmail.com>
2022-04-26 14:01:16 +0200

    Update Branch.java

--- src/Branch.java
+++ src/Branch.java
@@ -2,5 +2,13 @@
 public class Branch {
 
 	public int branchTotal;
+
+	public int getBranchTotal() {
+		return branchTotal;
+	}
+
+	public void setBranchTotal(int branchTotal) {
+		this.branchTotal = branchTotal;
+	}
 	
 }
